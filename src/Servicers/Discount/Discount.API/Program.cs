c3581022f8109c82478a0ba18b7b5eab0c099243
voi <lhpl@gmail.com>
2022-11-21 11:46:22 +0700

    Discount API migrate discountdb while starting

--- src/Servicers/Discount/Discount.API/Program.cs
+++ src/Servicers/Discount/Discount.API/Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Discount.API.Extensions;
 
 namespace Discount.API
 {
@@ -13,7 +14,10 @@ namespace Discount.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            host.MigrateDatabase<Program>(retry: 0);
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
