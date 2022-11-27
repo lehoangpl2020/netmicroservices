8fc461bb02fefa94913a7cc45204d51aa212b7fc
voi <lhpl@gmail.com>
2022-11-27 10:27:28 +0700

    Rabbit MQ - add mass transit to BasketAPI

--- src/Servicers/Basket/Basket.API/Startup.cs
+++ src/Servicers/Basket/Basket.API/Startup.cs
@@ -1,6 +1,7 @@
 using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Discount.Grpc.Protos;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -44,6 +45,18 @@ namespace Basket.API
                         (o => o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
 
             services.AddScoped<DiscountGrpcService>();
+
+
+            // MassTransit-RabbitMQ Configuration
+            services.AddMassTransit(config => {
+                config.UsingRabbitMq((ctx, cfg) => {
+                    cfg.Host(Configuration["EventBusSettings:HostAddress"]);
+                    //cfg.UseHealthCheck(ctx);
+                });
+            });
+            services.AddMassTransitHostedService();
+
+
 
 
             services.AddControllers();
